0c930dc1ec4e64ce64f51c8fc08cca6c911ae528
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-05 22:11:51 +0100

    Completata parte bonus e frecce tastiera

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
-    $('.next').click(nextImage);
     $('.prev').click(prevImage);
+    $('.next').click(nextImage);
+
+    /* Funzione per utilizzare tasti left e right della tastiera */
+    $(document).keydown(function(key) {
+        if (key.which == 37) {
+            prevImage();
+        } else if (key.which == 39) {
+            nextImage();
+        }
+    });
 
     /* Con un ciclo for aggiungo i circle per quante sono le immagini inserite */
     for (var i = 0; i < $(".images img").length; i++) {
@@ -11,18 +20,19 @@ $(document).ready(function() {
     $("#nav i:first-child").addClass('active first');
     $("#nav i:last-child").addClass('last');
 
+    $('#nav i').click(circleClick);
 });
 
 function nextImage() {
     var activeImage = $('.images .active');
     activeImage.removeClass('active');
 
-    var activeCircle = $('#nav .active');
+    var activeCircle = $('#nav i.active');
     activeCircle.removeClass('active');
     
     if (activeImage.hasClass('last')) {
         $('.images .first').addClass('active');
-        $('#nav .first').addClass('active');
+        $('#nav i.first').addClass('active');
     } else {
         activeImage.next('img').addClass('active');
         activeCircle.next('i').addClass('active');
@@ -33,14 +43,26 @@ function prevImage() {
     var activeImage = $('.images .active');
     activeImage.removeClass('active');
 
-    var activeCircle = $('#nav .active');
+    var activeCircle = $('#nav i.active');
     activeCircle.removeClass('active');
 
     if (activeImage.hasClass('first')) {
         $('.images .last').addClass('active');
-        $('#nav .last').addClass('active');
+        $('#nav i.last').addClass('active');
     } else {
         activeImage.prev('img').addClass('active');
         activeCircle.prev('i').addClass('active');
     }
+}
+
+function circleClick() {
+
+    /* Rimuovo la classe active a tutte le img e tramite l'indice 
+    del circle selezionato aggiungo classe active all'img corrispondente */
+    $('.images img').removeClass('active');
+    $('.images img').eq($('#nav i').index(this)).addClass('active');
+
+    /* Rimuovo classe active ai circle e l'aggiungo solo a quello selezionato */
+    $('#nav i').removeClass('active');
+    $(this).addClass('active');
 }
